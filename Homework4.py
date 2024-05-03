aa9c48d397d38981c0ebdf39f95dd6a591bef7ab
slishkombobr <168134177+slishkombobr@users.noreply.github.com>
2024-05-03 10:43:00 +0300

    Add files via upload

---
+++ Homework4.py
@@ -0,0 +1,11 @@
+immutable_var = tuple([1, 45, 'cup'])
+print(immutable_var)
+immutable_var [0] = 313
+
+
+
+
+mutable_list = [1, 45, "cup", True]
+print(mutable_list)
+mutable_list [1] = 313
+print(mutable_list)
