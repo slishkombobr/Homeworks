3e8d81eb3f3e15dc20ac2a5256f2bb714456ed30
slishkombobr <168134177+slishkombobr@users.noreply.github.com>
2024-05-05 21:29:13 +0300

    Add files via upload

---
+++ Homework5.py
@@ -0,0 +1,16 @@
+my_list = ['mango', 'peach', 'apple', 'kiwi', 'orange', 'banana', 'pear']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'grape'
+print(my_list)
+
+my_dict = {'mango': 'манго', 'peach': 'персик', 'apple': 'яблоко', 'kiwi': 'киви', 'orange': 'апельсин', 'banana': 'банан'}
+print(my_dict)
+print(my_dict['kiwi'])
+my_dict['pear'] = 'груша'
+print(my_dict)
+my_dict.pop('orange')
+print(my_dict)
+
