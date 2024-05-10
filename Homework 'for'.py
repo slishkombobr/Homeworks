45bb272eea8691335ab41a152537dec0360cd541
slishkombobr <168134177+slishkombobr@users.noreply.github.com>
2024-05-10 19:44:58 +0300

    Update Homework 'for'.py

--- Homework 'for'.py
+++ Homework 'for'.py
@@ -1,12 +1,8 @@
 cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
-for i in cars:
-     print('Я езжу на машине марки', '', i)
-
-
-cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
 cars_count = 0
 for i in cars:
     cars_count += 10
+    print('Я езжу на машине марки', i)
     print(cars_count)
 
 
