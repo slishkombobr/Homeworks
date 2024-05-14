d71697b6201419122ee3d913dfe14402fda5f6c4
slishkombobr <168134177+slishkombobr@users.noreply.github.com>
2024-05-14 16:42:57 +0300

    Add files via upload

---
+++ Homework 'def'.py
@@ -0,0 +1,6 @@
+def print_params(name,lastname):
+    print('Hello,', name, lastname)
+
+print_params('Svetlana','Domanskaya')
+print_params('Inna','Kirsanova')
+print_params('Maria','Nikulchenkova')
