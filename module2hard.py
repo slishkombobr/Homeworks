15f277e938306d823fc65235f701cfce8c09882a
slishkombobr <168134177+slishkombobr@users.noreply.github.com>
2024-05-15 22:38:47 +0300

    Update module2hard.py

--- module2hard.py
+++ module2hard.py
@@ -1,7 +1,11 @@
+result = ''
+
 n = int(input('Введите число от 3 до 20: '))
 
-for i in range(1,21):
-    for x in range(i):
-        for y in range(i):
-            if n % (x+y) == 0:
-                print( n, 'это', i)
+for x in range(1, 21):
+    for y in range(1, 21):
+        if n % (x + y) == 0 and (x + y) <= n and x < y:
+            result += str(x) + str(y)
+
+
+print(n, '-', result)
